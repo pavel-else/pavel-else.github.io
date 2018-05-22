abfe960b916d42af2f94d0d728c4648f6222f18c
pavel-else <de.pavel.se@yandex.ru>
2018-05-22 15:25:12 +0300

    update timeFunction

--- rental/main.js
+++ rental/main.js
@@ -2,33 +2,29 @@ new Vue({
 	el: '.sample',
 	data: {
 		showOrderModal: false,
-		order: {
-			id: Number,
-			name: String,
-			customer: String,
-			time: Date,
-			timeStart: String,
-			timePlay: Date,
-			timeEnd: Number,
-			timeFull: Number,
-			bill: Boolean,
-			note: String,
-			saleId: Number,
-		},
+		order: {},
 
 		products: false,
 		orders: []
 	},
 	methods: {
 		toEdit(item) {
+			this.clearOrder();
 			this.showOrderModal = true;
 			this.order.name = item.name;
+		},
+		setOrder() {
 			let time = new Date();
 			this.order.time = time;
-			this.timeStart = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate() + ' ' + time.getHours() + ':' + time.getSeconds();
-			console.log(this.timeStart)
-		},
+			this.order.timeStart = time.toLocaleString();
+			this.order.timePlay = 0;
 
+			this.orders.push(this.order);
+			this.clearOrder();
+
+			this.showOrderModal = false;
+			this.products.splice(this.products.indexOf(name), 1);
+		},
 		unset(item, index) {
 			this.products.push(item);
 			this.orders.splice(index, 1);
@@ -70,16 +66,45 @@ new Vue({
 				},
 			]
 		},
-	},
-	computed: {
-		setOrder() {
-			this.orders.push(this.order);
-			this.showOrderModal = false;
-			this.products.splice(this.products.indexOf(name), 1);
+		clearOrder() {
+			this.order = {
+				id: Number,
+				name: String,
+				customer: String,
+				time: Date,
+				timeStart: String,
+				timePlay: Date,
+				timeEnd: Number,
+				timeFull: Number,
+				bill: Boolean,
+				note: String,
+				saleId: Number,
+			}
+		},
+		timeFormat (ms/**number*/){
+		    function num(val){
+		        val = Math.floor(val);
+		        return val < 10 ? '0' + val : val;
+		    }
+		    
+	        var sec = ms / 1000
+	          , hours = sec / 3600  % 24
+	          , minutes = sec / 60 % 60
+	          , seconds = sec % 60
+	        ;
+
+		    return num(hours) + ":" + num(minutes) + ":" + num(seconds);
 		},
 	},
 	created() {
 		this.products = this.getData();
 		//Нужно добавить обработку возможных ошибок
+		
+		setInterval(function(orders) {
+			for (let order = 0; order < orders.length; order++) {
+				orders[order].timePlay = new Date() - orders[order].time;
+			}
+		}, 1000, this.orders);
 	}
+
 })
