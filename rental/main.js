3d6455ee5ad7d8548fe911fdb241c7d6231ad17c
pavel-else <de.pavel.se@yandex.ru>
2018-05-22 14:32:16 +0300

    add rental э

---
+++ rental/main.js
@@ -0,0 +1,85 @@
+new Vue({
+	el: '.sample',
+	data: {
+		showOrderModal: false,
+		order: {
+			id: Number,
+			name: String,
+			customer: String,
+			time: Date,
+			timeStart: String,
+			timePlay: Date,
+			timeEnd: Number,
+			timeFull: Number,
+			bill: Boolean,
+			note: String,
+			saleId: Number,
+		},
+
+		products: false,
+		orders: []
+	},
+	methods: {
+		toEdit(item) {
+			this.showOrderModal = true;
+			this.order.name = item.name;
+			let time = new Date();
+			this.order.time = time;
+			this.timeStart = time.getFullYear() + '-' + (time.getMonth() + 1) + '-' + time.getDate() + ' ' + time.getHours() + ':' + time.getSeconds();
+			console.log(this.timeStart)
+		},
+
+		unset(item, index) {
+			this.products.push(item);
+			this.orders.splice(index, 1);
+		},
+		getData() {
+			//Эммитирует запрос к БД
+			return [
+				{
+					id: 1,
+					name: 'GT Laguna [White]',
+					img: '',
+					cost: 16500,
+					active: true,
+					tarif_id: 1,
+				},
+				{
+					id: 2,
+					name: 'Mongoose Fireball',
+					img: '',
+					cost: 20000,
+					active: true,
+					tarif_id: 1,
+				},
+				{
+					id: 1,
+					name: 'Stels 530',
+					img: '',
+					cost: 8000,
+					active: true,
+					tarif_id: 2,
+				},
+				{
+					id: 1,
+					name: 'BMX',
+					img: '',
+					cost: 7000,
+					active: true,
+					tarif_id: 2,
+				},
+			]
+		},
+	},
+	computed: {
+		setOrder() {
+			this.orders.push(this.order);
+			this.showOrderModal = false;
+			this.products.splice(this.products.indexOf(name), 1);
+		},
+	},
+	created() {
+		this.products = this.getData();
+		//Нужно добавить обработку возможных ошибок
+	}
+})
